e4eb5cd656d096320e723e7922f0ed1d3690c729
Sarah Reuter <sreuter1@gmail.com>
2024-11-28 14:29:33 -0800

    final product no challenges

--- fileutil.h
+++ fileutil.h
@@ -7,9 +7,6 @@
 // entries in the array (not the total array length).
 char ** loadFileAA(char *filename, int *size);
 
-char (*loadFile2D(char *filename, int *size))[COLS];
-
-
 // Search the dictionary for the target string
 // Return the found string or NULL if not found.
 char * substringSearchAA(char *target, char ** arr, int size);
